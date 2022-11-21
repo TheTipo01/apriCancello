fe2ca7846755e43194e106346cae45c264241829
TheTipo01 <thetipo01@protonmail.com>
2022-11-21 21:29:19 +0100

    feat: esp8266 code added

---
+++ esp8266_code/example_credentials.h
@@ -0,0 +1,6 @@
+// Change them to your WiFi credentials
+const char* ssid = "5G Antenna";
+const char* password = "very_secure_password";
+
+// Modify the key to something more secure
+const char* endpoint = "/open?key=loremipsum";
